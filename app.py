77a5d5a5643140fd66d530d45929c0c235bf9850
Quentin Lintz <quentinlintz@outlook.com>
2022-03-22 18:50:30 -0400

    Add tweepy and create tweet from Google Analytics data

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from settings import PROPERTY_ID
+import math
+import tweepy
+from settings import * 
 from google.analytics.data_v1beta import BetaAnalyticsDataClient
 from google.analytics.data_v1beta.types import DateRange
 from google.analytics.data_v1beta.types import Dimension
@@ -8,7 +10,12 @@ from google.analytics.data_v1beta.types import FilterExpressionList
 from google.analytics.data_v1beta.types import Metric
 from google.analytics.data_v1beta.types import RunReportRequest
 
+WIDTH = 10
 game_stats_dict = {}
+tweet = ''
+
+# Google Analytics API
+
 client = BetaAnalyticsDataClient()
 
 request = RunReportRequest(
@@ -21,7 +28,7 @@ request = RunReportRequest(
                     filter=Filter(
                         field_name="eventName",
                         in_list_filter=Filter.InListFilter(
-                            values=["wami victory", "wami game over"]
+                            values=["wami_victory", "wami_game_over"]
                         )
                     )
                 ),
@@ -29,7 +36,7 @@ request = RunReportRequest(
                     filter=Filter(
                         field_name="pageTitle",
                         string_filter=Filter.StringFilter(
-                            value="WAMI - Guess the Word - Quentin Lintz"),
+                            value="WAMI - Guess the Word"),
                     )
                 ),
             ]
@@ -45,6 +52,24 @@ response = client.run_report(request)
 for row in response.rows:
   game_stats_dict[row.dimension_values[0].value] = row.metric_values[0].value
 
-# {'wami victory': '5'}
-print(game_stats_dict)
+# Create Tweet
 
+victory = int(game_stats_dict['wami_victory'])
+game_over = int(game_stats_dict['wami_game_over'])
+total_players = victory + game_over
+victory_percent = round((victory / total_players * 100), 2)
+game_over_percent = round((game_over / total_players * 100), 2)
+victory_emoji_count = math.floor((victory / total_players) * WIDTH)
+game_over_emoji_count = math.ceil((game_over / total_players) * WIDTH)
+
+tweet = '🤖 WAMI statistics for today are in! 🤖 \n \
+' + str(total_players) + ' people played WAMI today\n \
+' + str(victory_percent) + '% got the answer correct!\n \
+' + '🟩'* victory_emoji_count + '🟥' * game_over_emoji_count + '\n \
+' + 'Well played! New round is starting right now...\n \
+' + '#WAMI #wordgame #bot #statistics'
+
+# Twitter API
+
+client = tweepy.Client(bearer_token=BEARER_TOKEN, consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET, access_token=ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET)
+response = client.create_tweet(text=tweet)
